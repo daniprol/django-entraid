706058c60bdb7aa2ade0c3cdcb652ff05a045b4c
Dani Prol <prolcambados@gmail.com>
2025-02-10 23:41:07 +0100

    API private view with auth

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import CommentViewSet, home, private_view
+from .views import CommentViewSet, home, private_api_view, private_view
 
 router = DefaultRouter()
 router.register(r"comment/", CommentViewSet)
 
 
-urlpatterns = [path("", home), path("private/", private_view)]
+urlpatterns = [
+    path("", home),
+    path("private/", private_view),
+    path("api/", private_api_view),
+]
 urlpatterns += router.urls
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from rest_framework import generics, viewsets
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Comment
 from .serializers import CommentSerializer
@@ -10,7 +12,8 @@ def home(request):
     return HttpResponse("Hello world")
 
 
-@login_required()
+# NOTE: login_required() also works
+@login_required
 def private_view(request):
     return HttpResponse("Private")
 
@@ -18,3 +21,9 @@ def private_view(request):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def private_api_view(request):
+    return HttpResponse("API private view")
